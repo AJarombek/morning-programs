278e8364dafb1bf943d163877fae161012d1c11d
ajarombek <ajarombek95@gmail.com>
2022-04-23 20:33:19 -0400

    diary intermission

--- 2022/04/23/jewelsAndStones.ts
+++ 2022/04/23/jewelsAndStones.ts
@@ -4,6 +4,17 @@
  * @since 4/23/2022
  */
 
+// If I’m wrong, I’m sorry.  It’s been so long, it’s difficult for my mind to find explanations
+// other than you not wanting to speak to me again,
+// even as my heart tries to hold on to words and letters and feelings from long ago.
+
+// If I’m wrong, please, somehow, someway, trust me with the truth.  I want to live my life with the truth
+// there to guide me.  I love you.  You, Jacqui, are the kindest person I have met in my life, and you’ve treated me
+// so well and made me feel so loved (even though I know you feel like you haven't always).  The truth of your life,
+// your feelings, my life, my feelings, is what will get us where we want to go.
+
+// If I was correct in what I said yesterday, you can ignore me.  What I want most of all is your happiness.
+
 function numJewelsInStones(jewels: string, stones: string): number {
     let set = new Set<string>();
 
